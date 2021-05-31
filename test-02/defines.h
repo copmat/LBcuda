e7f04abced09dae11ed67b50976e161446d89c1a
Marco Lauricella <mlaurice@login02.m100.cineca.it>
2021-05-31 13:12:47 +0200

    test cases added

---
+++ test-02/defines.h
@@ -0,0 +1,12 @@
+#define BGK
+#define CG
+#define PARTICLES
+
+#define noDEBUG_FORCE
+#define noDEBUG_ROT
+#define noDEBUG_MKRM
+#define noDEBUG_N2P
+#define noDEBUG_P2N
+
+
+#define MINDENS   0.e-8
